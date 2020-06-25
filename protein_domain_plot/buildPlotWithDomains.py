50c707728117f271284c36d6b2f46af5e55649b7
hossam-zaki <hossam_zaki@brown.edu>
2020-06-24 22:44:53 -0400

    get's start and end from the output of the R script

--- protein_domain_plot/buildPlotWithDomains.py
+++ protein_domain_plot/buildPlotWithDomains.py
@@ -2,17 +2,23 @@ import re
 import subprocess
 
 result = subprocess.run(
-    ["Rscript", "protein_domain_plot/getDomains.R", "--start", "500", "--width", "30", "--transcript", "ENST00000487861"], stdout=subprocess.PIPE, text=True)
+    ["Rscript", "protein_domain_plot/getDomains.R", "--start", "500", "--width", "50", "--transcript", "ENST00000487861"], stdout=subprocess.PIPE, text=True)
 
 str = result.stdout.split('\n')
 
 start = []
+bool = False
 for sub in str:
-    start.append(re.match(".*ENSP00000419881\s+(\d+)\s+(\d+).*", sub))
-
+    print(bool)
+    if bool:
+        start.append(re.match(".+\s+(\d+).+", sub)[1])
+        bool = False
+    if '<integer> <logical>' in sub:
+        bool = True
+        continue
+    match = re.match(".+ENST00000487861\s+(\d+).*", sub)
+    if match:
+        start.append(match[1])
 for i in start:
     if i:
-        print("yee")
-        print(i[1])
-        print("yee2")
-        print(i[2])
+        print(i)
