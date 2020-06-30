0ecb62c875e8914e1caed81e91fe29f06b5a0d2c
hossam-zaki <hossam_zaki@brown.edu>
2020-06-29 23:42:22 -0400

    updated color scheme

--- protein_domain_plot/helperFiles/buildplot.py
+++ protein_domain_plot/helperFiles/buildplot.py
@@ -16,7 +16,7 @@ def lift(coord):
     return lo.convert_coordinate('chr14', coord)[0][1]
 
 
-def get_cmap(n, name='hsv'):
+def get_cmap(n, name='Accent'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
