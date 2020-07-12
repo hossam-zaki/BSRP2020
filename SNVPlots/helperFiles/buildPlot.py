e892eb9be0cbccad3432452ba62cab73fd5a1fa0
hossam-zaki <hossam_zaki@brown.edu>
2020-07-12 02:50:27 -0400

    starting SNV Plots

---
+++ SNVPlots/helperFiles/buildPlot.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import, print_function
+
+import json
+import urllib
+
+import icgc
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import pandas as pd
+from pyliftover import LiftOver
+
+lo = LiftOver('hg19', 'hg38')
+
+
+def lift(coord, chromosome):
+    chromo = 'chr' + str(chromosome)
+    return lo.convert_coordinate(chromo, coord)[0][1]
+
+
+def get_cmap(n, name='Accent'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+
+def buildPlot():
+    print("getting number of seqs...")
+    numberOfSeqs = json.load(urllib.request.urlopen(
+        "http://dcc.icgc.org/api/v1/genes/ENSG00000182185/mutations/count"))
+    print("getting start and end...")
+    startAndEndJson = json.load(urllib.request.urlopen(
+        "https://dcc.icgc.org/api/v1/genes/ENSG00000182185"))  # lift this
+    start = lift(startAndEndJson['start'])
+    end = lift(startAndEndJson['end'])
+    counter = 0
+    to_nearest_hunderd = 101 - (numberOfSeqs % 100)
+    placeInGenome = []
+    numberOfOccurences = []
+    width = []
+    #
+    for i in range(0, numberOfSeqs+to_nearest_hunderd, 100):
+        try:
+            json_file = json.load(urllib.request.urlopen(
+                f"https://dcc.icgc.org/api/v1/genes/ENSG00000182185/mutations?from={i}&size=100"))
+        except:
+            print(f"{i} got messed")
+        print(i)
+
+        for hit in json_file['hits']:
+            if hit['type'] == 'single base substitution':
+                continue
+            placeInGenome.append(lift(hit['start'])-start)  # lift this
+            width.append((hit['end'] - hit['start'])+1)
+            numberOfOccurences.append(
+                hit['affectedDonorCountTotal']/hit['testedDonorCount'])
+    print(width)
+    plt.bar(placeInGenome, numberOfOccurences,
+            width=width, align='edge')
+    plt.title("RAD51B Mutation Map")
+    plt.xlabel('Position')
+    plt.ylabel('Frequency of Mutation')
+    plt.show()
+
+
+if __name__ == "__main__":
+    buildPlot()
---
+++ SNVPlots/helperFiles/buildPlotWithDomains.py
@@ -0,0 +1,73 @@
+import json
+import re
+import subprocess
+import sys
+import urllib
+
+import ensembl_rest
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import buildPlot as plotBuilder
+
+
+def getProteinCodingRanges(start, end):
+    result = subprocess.run(
+        ["Rscript", "protein_domain_plot/getDomains.R", "--start", start, "--end", end], stdout=subprocess.PIPE, text=True)
+    str = result.stdout.split('\n')
+    toReturn = set()
+    for sub in str:
+        match = re.match(".*\[\d*\]\s*\d*.(\d*)-(\d*).*", sub)
+        if match:
+            toReturn.add((match[1], match[2]))
+    return toReturn
+
+
+def buildRanges():
+    transcriptAndRange = {}
+    ranges = proteinRanges.getRange()
+    for feat in ranges:
+        print(ranges[feat])
+        result = getProteinCodingRanges(ranges[feat][0], ranges[feat][1])
+        transcriptAndRange[feat] = []
+        for item in result:
+            transcriptAndRange[feat].append(item)
+    return transcriptAndRange
+
+
+def buildPlotWithProtein():
+    print('building plot...')
+    plotBuilder.buildPlot()
+    print("loading json...")
+    startAndEndJson = json.load(urllib.request.urlopen(
+        "https://dcc.icgc.org/api/v1/genes/ENSG00000182185"))  # lift this
+    start = plotBuilder.lift(startAndEndJson['start'])
+    end = plotBuilder.lift(startAndEndJson['end'])
+    print(start)
+    ranges = buildRanges()
+    cmap = plotBuilder.get_cmap(len(ranges)+1)
+    counter = 1
+    legend = []
+    ax = plt.gca()
+    for feat in ranges:
+        for transcript in ranges[feat]:
+            print(feat)
+            print((int(transcript[0])-start),
+                  (int(transcript[1])-start))
+            rect = patches.Rectangle(
+                (int(transcript[0])-start, -counter/500), height=1/500,
+                width=((int(transcript[1])-start) - (int(transcript[0])-start)), fill=True, color=cmap(counter-1))
+            ax.add_patch(rect)
+
+        legend.append(patches.Patch(color=cmap(counter-1), label=feat))
+        counter += 1
+    axes = plt.gca()
+    axes.set_xlim([0, end-start])
+    axes.set_ylim([-counter/500, .05])
+    plt.legend(handles=legend,)
+    plt.show()
+
+
+if __name__ == "__main__":
+    buildPlotWithProtein()
