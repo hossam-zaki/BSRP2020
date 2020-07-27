7b8714df2ecf87c6a371e184b78310f97e3034b4
hossam-zaki <hossam_zaki@brown.edu>
2020-07-26 21:42:50 -0400

    length figures and code fully added

--- SVPlots/lengthOfSVs.py
+++ SVPlots/lengthOfSVs.py
@@ -15,58 +15,77 @@ df = pd.read_csv('../merged_1.6.1.csv')
 
 data = []
 labels = []
-#
+
 labels = parser.load_obj('labels')
+data = parser.load_obj('lengthData')
 
+print(stats.f_oneway(*data))
 
-for gene in labels:
-    print(gene)
-    try:
-        geneDF, chromsome = parser.getGeneDF(gene)
+for i in range(1, len(labels)):
+    label = labels[i]
+    print(
+        f"p-value for RAD51B and {label}: {stats.ttest_ind(data[20], data[i])}")
 
-        geneData = []
-
-        for index, row in geneDF.iterrows():
-            if (int(row['seqnames']) == chromsome) and (int(row['altchr']) == chromsome):
-                geneData.append(int(row['altpos']) - int(row['start']))
-
-        data.append(geneData)
-    except:
-        continue
-
-ind = np.arange(start=0, stop=len(data)*1.5, step=3)
-width = 1.25  # the width of the bars
+ind = np.arange(start=0, stop=len(data) * 2, step=2)
+width = 1.5  # the width of the bars
 fig2, ax2 = plt.subplots(figsize=(20, 15))
 means = []
 stdevs = []
-print(stats.f_oneway(*data))
 for dataset in data:
+    if len(dataset) == 0:
+        means.append(0)
+        continue
     means.append(mean(dataset))
 for dataset in data:
     try:
         stdevs.append(stdev(dataset))
     except:
         stdevs.append(0)
-rects1 = ax2.bar(ind, means, width,
-                 label='WT Gene', yerr=stdevs, zorder=0)
+print(len(ind))
+print(len(means))
+rects1 = ax2.bar(ind, means, width, yerr=stdevs, zorder=0)
 for i in range(0, len(ind)):
     # distribute scatter randomly across whole width of bar
     ax2.scatter((ind[i]) + np.random.rand(len(data[i])) *
-                width, data[i], color='black', zorder=10, s=10)
-ax2.set_ylabel('Number of SVs')
-ax2.set_title('Mean number of SVs in WT vs Mutant Genes')
+                width - width/2, data[i], color='black', zorder=10, s=10)
+ax2.set_ylabel('Length in bp')
+ax2.set_xlabel('Gene')
+ax2.set_title('Mean Length of Intrachromosomal Rearrangements')
 ax2.set_xticks(ind)
 ax2.set_xticklabels(labels)
 plt.xticks(rotation=90)
-plt.scatter([], [], marker=r'$\ast$', label="p < .05",
+plt.scatter([], [], marker='.', label="Length of an individual SV",
             color='black', linestyle='None')
-plt.scatter([], [], marker=r'$\ast\ast$', label="p < .01",
-            color='black', linestyle='None', s=600)
-plt.scatter([], [], marker=r'$\ast\ast\ast$',
-            label="p < .005", color='black', linestyle='None', s=750)
 ax2.legend()
-autolabel(rects2, pValues)
-plt.show()
 
+plt.show()
+forBarPlot = []
+for gene in labels:
+    print(gene)
+    try:
+        geneDF, chromsome = parser.getGeneDF(gene)
+
+        geneData = 0
+
+        for index, row in geneDF.iterrows():
+            if (int(row['seqnames'])) != (int(row['altchr'])):
+                geneData += 1
+        forBarPlot.append(geneData)
+    except:
+        forBarPlot.append(0)
+        continue
+
+print(forBarPlot)
+
+fig1, ax = plt.subplots(figsize=(10, 6))
+ax.set_title('Basic Plot')
+x = np.arange(len(labels))
+rects = ax.bar(x, forBarPlot)
+ax.set_ylabel('Number of Rearragements')
+ax.set_xlabel('Gene')
+ax.set_title('Number of Interchromosomal Rearrangements per gene')
+ax.set_xticks(x)
+plt.xticks(rotation=90)
+ax.set_xticklabels(labels)
 
 plt.show()
